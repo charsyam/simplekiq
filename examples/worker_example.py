aefc8ef393b8ceb03ccf2c7206eb98b3d80a2d69
charsyam <charsyam@naver.com>
2021-06-27 22:18:16 +0900

    Initial Commit

---
+++ examples/worker_example.py
@@ -0,0 +1,26 @@
+import redis
+from base import KiqQueue
+from event import EventBuilder
+from worker import Worker
+
+
+class MyEventWorker(Worker):
+    def __init__(self, queue, failed_queue):
+        super().__init__(queue, failed_queue)
+
+    def _process(self, event_type, value):
+        print(event_type, value)
+
+
+conn = redis.StrictRedis()
+queue = KiqQueue(conn, "my_test_job", True)
+failed_queue = KiqQueue(conn, "failed", True)
+
+event_builder = EventBuilder(queue)
+value = event_builder.emit("test_event", {"age": 13, "value": "test", "1": {"2": 2}}, 3)
+queue.enqueue(value)
+
+worker = MyEventWorker(queue, failed_queue)
+
+while True:
+    worker.process(True)
